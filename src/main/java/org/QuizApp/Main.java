4c257bfeb864aa01abf9583b0200e97e1a645ece
Vinod Kumar Botlagunta <87555754+VinodKumar-Botlagunta@users.noreply.github.com>
2023-10-24 21:35:55 +0530

    Update Main.java
    changes in Main file

--- src/main/java/org/QuizApp/Main.java
+++ src/main/java/org/QuizApp/Main.java
@@ -4,7 +4,7 @@ package org.QuizApp;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int noOfQuestions=1;
+        int noOfQuestions=5;
         QuizManager qm = new QuizManager(noOfQuestions);
         int result =qm.userResult();
         System.out.println("----------------------------------");
